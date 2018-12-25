1126d06d189d48ec19f467fce1384e0d1cc5cd5c
Gabor Nagypal <nagypalg@gmail.com>
2018-12-25 17:41:10 +0100

    added coroutines dependencies, added basic example

---
+++ src/main/kotlin/coroutines/Basics.kt
@@ -0,0 +1,13 @@
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val job = GlobalScope.launch { // launch new coroutine and keep a reference to its Job
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello,")
+    job.join() // wait until child coroutine completes
+}
